5fb88d749d012c3402ff868629d2d4e2e6df2add
AbdelrhmanY10 <abdelrhmanyasser123@gmail.com>
2021-07-22 23:27:29 +0200

    First Commit

--- lib/screens/add_posts/add_posts.dart
+++ lib/screens/add_posts/add_posts.dart
@@ -1,14 +1,167 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:social_app/layout/social_layout/cubit/cubit.dart';
+import 'package:social_app/layout/social_layout/cubit/states.dart';
 import 'package:social_app/shared/components/components.dart';
+import 'package:social_app/shared/styles/colors.dart';
 import 'package:social_app/shared/styles/icon_broken.dart';
 
+// ignore: must_be_immutable
 class AddPosts extends StatelessWidget {
+  var textController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: defaultAppBar(context: context,
-          title: "Add Post",
-      ),
+    return BlocConsumer<SocialCubit, SocialStates>(
+      listener: (context, state) {},
+      builder: (context, state) {
+        var userModel = SocialCubit.get(context).userModel;
+        return Scaffold(
+          appBar: defaultAppBar(
+            context: context,
+            title: 'Create Post',
+            actions: [
+              defaultTextButton(
+                function: ()
+                {
+                  var now = DateTime.now();
+
+                  if (SocialCubit.get(context).postImage == null)
+                  {
+                    SocialCubit.get(context).createPost(
+                      dateTime: now.toString(),
+                      text: textController.text,
+                    );
+                  } else
+                  {
+                    SocialCubit.get(context).uploadPostImage(
+                      dateTime: now.toString(),
+                      text: textController.text,
+                    );
+                  }
+                },
+                text: 'Post',
+              ),
+            ],
+          ),
+          body: Padding(
+            padding: const EdgeInsets.all(20.0),
+            child: Column(
+              children: [
+                if(state is SocialCreatePostLoadingState)
+                  LinearProgressIndicator(),
+                if(state is SocialCreatePostLoadingState)
+                  SizedBox(
+                    height: 10.0,
+                  ),
+                Row(
+                  children: [
+                    CircleAvatar(
+                      radius: 25.0,
+                      backgroundImage: NetworkImage(
+                        '${userModel!.image}',
+                      ),
+                    ),
+                    SizedBox(
+                      width: 15.0,
+                    ),
+                    Expanded(
+                      child: Text(
+                        '${userModel.name}',
+                        style: TextStyle(
+                          height: 1.4,
+                          fontWeight: FontWeight.bold,
+                          fontSize: 16.0
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+                Expanded(
+                  child: TextFormField(
+                    controller: textController,
+                    decoration: InputDecoration(
+                      hintText: 'what is on your mind ...',
+                      border: InputBorder.none,
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  height: 20.0,
+                ),
+                if(SocialCubit.get(context).postImage != null)
+                  Stack(
+                    alignment: AlignmentDirectional.topEnd,
+                    children: [
+                      Container(
+                        height: 140.0,
+                        width: double.infinity,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(4.0,),
+                          image: DecorationImage(
+                            image: FileImage(SocialCubit.get(context).postImage!),
+                            fit: BoxFit.cover,
+                          ),
+                        ),
+                      ),
+                      IconButton(
+                        icon: CircleAvatar(
+                          radius: 20.0,
+                          child: Icon(
+                            Icons.close,
+                            size: 16.0,
+                          ),
+                        ),
+                        onPressed: ()
+                        {
+                          SocialCubit.get(context).removePostImage();
+                        },
+                      ),
+                    ],
+                  ),
+                SizedBox(
+                  height: 20.0,
+                ),
+                Row(
+                  children: [
+                    Expanded(
+                      child: TextButton(
+                        onPressed: ()
+                        {
+                          SocialCubit.get(context).getPostImage();
+                        },
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Icon(
+                              IconBroken.Image,
+                            ),
+                            SizedBox(
+                              width: 5.0,
+                            ),
+                            Text(
+                              'Add Photo',
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                    Expanded(
+                      child: TextButton(
+                        onPressed: () {},
+                        child: Text(
+                          '# tags',
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
+        );
+      },
     );
   }
 }
